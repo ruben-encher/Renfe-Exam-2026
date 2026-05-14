2a36d2dee4083a0efac7b94bd59716e0a62703bf
EHR <ruben.encher@educa.jcyl.es>
2026-05-14 16:46:16 +0200

    Feature 4 - Continuación de la arquitectura (archivos y código)

---
+++ src/main/java/edu/iesam/trains/data/TrinDataRepository.java
@@ -0,0 +1,8 @@
+package edu.iesam.trains.data;
+
+import edu.iesam.trains.domain.TrainRepository;
+
+public class TrinDataRepository implements TrainRepository {
+
+
+}
---
+++ src/main/java/edu/iesam/trains/domain/TrainRepository.java
@@ -0,0 +1,4 @@
+package edu.iesam.trains.domain;
+
+public interface TrainRepository {
+}
