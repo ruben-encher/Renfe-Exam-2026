be2602b4d262266c58761a5723350c1acf8e3f20
EHR <ruben.encher@educa.jcyl.es>
2026-05-14 17:40:39 +0200

    Feature 16 - Mocks completos para poder realizar test.

--- src/test/java/edu/iesam/trains/mock/EmptyMockRepository.java
+++ src/test/java/edu/iesam/trains/mock/EmptyMockRepository.java
@@ -1,4 +1,24 @@
 package edu.iesam.trains.mock;
 
-public class EmptyMockRepository {
+import edu.iesam.trains.domain.Train;
+import edu.iesam.trains.domain.TrainRepository;
+
+import java.util.ArrayList;
+
+public class EmptyMockRepository implements TrainRepository {
+    @Override
+    public void add(Train train) {
+
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+
+    @Override
+    public ArrayList<Train> getAll() {
+        ArrayList<Train> trains = new ArrayList<>();
+        return trains;
+    }
 }
--- src/test/java/edu/iesam/trains/mock/TrainMockRepository.java
+++ src/test/java/edu/iesam/trains/mock/TrainMockRepository.java
@@ -1,4 +1,33 @@
 package edu.iesam.trains.mock;
 
-public class TrainMockRepository {
+import edu.iesam.trains.domain.Train;
+import edu.iesam.trains.domain.TrainRepository;
+
+import java.util.ArrayList;
+
+public class TrainMockRepository implements TrainRepository {
+
+    public Integer coundAddTrain = 0;
+
+    @Override
+    public void add(Train train) {
+        coundAddTrain++;
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+
+    @Override
+    public ArrayList<Train> getAll() {
+        ArrayList<Train> trains = new ArrayList<>();
+        Train train = new Train("1", "100","200/km","Avila");
+        Train train2 = new Train("2", "150","220/km","Madrid");
+        Train train3 = new Train("3", "160","180/km","Valencia");
+        trains.add(train);
+        trains.add(train2);
+        trains.add(train3);
+        return trains;
+    }
 }
